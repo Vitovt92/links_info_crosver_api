be9e3471fa53a0875c58e2bb6db200d619758fc5
vitovt <vitovt92@rambler.ru>
2020-06-21 23:59:11 +0300

    Added .env environment, simple docker file,

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import dotenv from "dotenv";
 import express from "express";
-const app = express();
 
-const port = 8081;
+//initialize configuration
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
